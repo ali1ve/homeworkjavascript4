cace8ff033108a9ecff3576c1066f238221a34bf
ali1ve <aliaga.yahya@gmail.com>
2024-10-28 16:52:31 +0400

    homework4

---
+++ app.js
@@ -0,0 +1,12 @@
+let firstName = "Kərim"
+let lastName = "Qasimov"
+
+let fullName = firstName +" "+ lastName
+console.log(fullName);
+
+
+fullName = 
+ firstName.replaceAll("Kərim","Qasimov") +" "+ lastName.replaceAll("Qasimov","Kərim")
+
+console.log(fullName);
+
